9dc41f3412189028c84c7e2bd790209e7b9f45d5
Remington Davenport <remington.davenport@gmail.com>
2015-02-07 22:00:46 -0500

    loading levels from json files; using swift extensions

--- CookieCrunch/Level.swift
+++ CookieCrunch/Level.swift
@@ -13,11 +13,33 @@ let NumRows = 9
 
 class Level {
     private var cookies = Array2D<Cookie>(columns: NumColumns, rows: NumRows)
+    private var tiles = Array2D<Tile>(columns: NumColumns, rows: NumRows)
+    
+    init(filename: String) {
+        if let dictionary = Dictionary<String, AnyObject>.loadJSONFromBundle(filename) {
+            if let tilesArray: AnyObject = dictionary["tiles"] {
+                for (row, rowArray) in enumerate(tilesArray as [[Int]]) {
+                    let tileRow = NumRows - row - 1
+                    for (column, value) in enumerate(rowArray) {
+                        if value == 1 {
+                            tiles[column, tileRow] = Tile()
+                        }
+                    }
+                }
+            }
+        }
+    }
     
     func cookieAtColumn(column: Int, row: Int) -> Cookie? {
         assert(column >= 0 && column < NumColumns) // or we could just use an actual two-dimensional array...
         assert(row >= 0 && row < NumRows) // will cause app to crash if this fails
         return cookies[column, row]
+    }
+    
+    func tileAtColumn(column: Int, row: Int) -> Tile? {
+        assert(column >= 0 && column < NumColumns)
+        assert(row >= 0 && row < NumRows)
+        return tiles[column, row]
     }
     
     func shuffle() -> Set<Cookie> {
@@ -30,12 +52,14 @@ class Level {
         for row in 0..<NumRows {
             for column in 0..<NumColumns {
                 
-                var cookieType = CookieType.random()
+                if tiles[column, row] != nil {
+                    var cookieType = CookieType.random()
                 
-                let cookie = Cookie(column: column, row: row, cookieType: cookieType)
-                cookies[column, row] = cookie
+                    let cookie = Cookie(column: column, row: row, cookieType: cookieType)
+                    cookies[column, row] = cookie
                 
-                set.addElement(cookie)
+                    set.addElement(cookie)
+                }
             }
         }
         return set
---
+++ CookieCrunch/Levels/Tile.swift
@@ -0,0 +1,11 @@
+//
+//  Tile.swift
+//  CookieCrunch
+//
+//  Created by Remington Davenport on 2/7/15.
+//  Copyright (c) 2015 Remington Davenport. All rights reserved.
+//
+
+class Tile {
+    
+}
